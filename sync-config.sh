ea60be4d3280a7b1bd22ba4d072805c94dca15cf
Lance R. Vick <lance@lrvick.net>
2016-03-19 02:12:01 +0000

    Decryption testing/working

--- sync-config.sh
+++ sync-config.sh
@@ -6,31 +6,32 @@ cp /home/orbit/.rclone.conf{.template,}
 [ -z "${ENV_SOURCE}" ] && echo "No ENV_SOURCE URI specified" && exit
 
 if [[ "$ENV_SOURCE" == "s3://"* ]]; then
-	rclone sync s3:${ENV_SOURCE#s3://} /config
+        rclone sync s3:${ENV_SOURCE#s3://} /config                 
 elif [[ "$ENV_SOURCE" == "file://"* ]]; then
-	cp -R ${ENV_SOURCE#file://} /config
+        cp -R ${ENV_SOURCE#file://} /config
 else
-	echo "ENV_SOURCE URI not defined or invalid"
-	exit
+        echo "ENV_SOURCE URI not defined or invalid"
+        exit
 fi
 
 if [ ! -f /config/private.key ]; then
-	echo "No private key found, unable to decrypt"
+        echo "No private key found, unable to decrypt"
 else
-	gpg2 --import /config/private.key 2>/dev/null
+        gpg2 --import /config/private.key 2>/dev/null
 fi
 if [ ! -f /config/public.key ]; then
-	echo "No public key found, unable to verify signatures"
+        echo "No public key found, unable to verify signatures"
 else
-	gpg2 --import /config/public.key 2>/dev/null
+        gpg2 --import /config/public.key 2>/dev/null
 fi
 
 rm -f /config/public.key /config/private.key
 
-gpg_template='-----BEGIN PGP MESSAGE-----\n\n%s\n-----END PGPMESSAGE-----\n'
+gpg_template='-----BEGIN PGP MESSAGE-----\n\n%s\n\n-----END PGPMESSAGE-----\n'
 find /config -type f -iname "*.secret" -print0 \
   | while IFS= read -r -d $'\0' file; do
-	local out_file=$(echo $file | sed 's/.secreti$//g')
-	printf -- $gpg_template $(cat $file) | gpg2 --decrypt > $out_file
+        out_file=$(echo $file | sed 's/.secret$//g')
+        printf -- "$gpg_template" $(cat $file) >> ${out_file}.gpg
+        gpg2 --decrypt ${out_file}.gpg > ${out_file} 2> /dev/null
+        rm ${out_file}.{gpg,secret}
 done
-
